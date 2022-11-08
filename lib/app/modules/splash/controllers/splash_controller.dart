58fbcd54c5881b9eca5c2939dfd93acb984b2a03
Syahrul A.F <afrizalsyahrul@gmail.com>
2022-11-08 11:53:58 +0700

    -check if already login

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unnecessary_overrides
 import 'package:get/get.dart';
+import 'package:maiden_employer/app/config/constants/preference_constant.dart';
 import 'package:maiden_employer/app/routes/app_pages.dart';
+import 'package:maiden_employer/app/shared/utils/preference_helper.dart';
 
 class SplashController extends GetxController {
   var isShownOnboarding = false.obs;
@@ -20,8 +22,13 @@ class SplashController extends GetxController {
   void onClose() {}
 
   init() {
-    Future.delayed(Duration(seconds: 3), () {
-      isShownOnboarding.value = true;
+    Future.delayed(Duration(seconds: 3), () async {
+      String token = await PreferenceHelper().get(key: PreferenceConstant.USER_TOKEN);
+      if (token.isEmpty) {
+        isShownOnboarding.value = true;
+      } else {
+        Get.offAllNamed(Routes.MAIN);
+      }
     });
   }
 
