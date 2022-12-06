e6b4fe7145e71f0945e1c68eb3fec2bb0fd1884b
Syahrul A.F <afrizalsyahrul@gmail.com>
2022-12-06 09:48:43 +0700

    -implemen flow valodasi otp

--- lib/app/modules/account/authentication/forgot_password/controllers/validate_email_forgot_password_controller.dart
+++ lib/app/modules/account/authentication/forgot_password/controllers/validate_email_forgot_password_controller.dart
@@ -1,33 +1,24 @@
 // ignore_for_file: unnecessary_overrides
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:maiden_employer/app/config/constants/preference_constant.dart';
+import 'package:maiden_employer/app/data/repository/api_repositories.dart';
+import 'package:maiden_employer/app/models/response_resend_otp.dart';
+import 'package:maiden_employer/app/models/response_validate_otp.dart';
 import 'package:maiden_employer/app/routes/app_pages.dart';
 import 'package:maiden_employer/app/shared/common/common_function.dart';
+import 'package:maiden_employer/app/shared/utils/my_helper.dart';
+import 'package:maiden_employer/app/shared/utils/preference_helper.dart';
 
 class ValidateEmailForgotPasswordController extends GetxController {
   TextEditingController inputOTP = TextEditingController();
-  late Timer timer;
-  RxInt start = 0.obs;
+  RxString email = "".obs;
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
-    start.value = 30;
-    const oneSec = Duration(seconds: 1);
-    timer = Timer.periodic(
-      oneSec,
-      (Timer timer) {
-        if (start.value == 0) {
-          timer.cancel();
-        } else {
-          start--;
-        }
-        printInfo(info: start.value.toString());
-      },
-    );
+    email.value = MyHelpers.encryptEmailText(await PreferenceHelper().get(key: PreferenceConstant.USER_EMAIL));
   }
 
   @override
@@ -38,41 +29,54 @@ class ValidateEmailForgotPasswordController extends GetxController {
   @override
   void onClose() {
     super.onClose();
-    timer.cancel();
   }
 
   @override
   void dispose() {
     super.dispose();
-    timer.cancel();
   }
 
-  void startTimer() async {
-    if (start.value == 0) {
-      start.value = 30;
-      const oneSec = Duration(seconds: 1);
-      timer = Timer.periodic(
-        oneSec,
-        (Timer timer) {
-          if (start.value == 0) {
-            timer.cancel();
-          } else {
-            start--;
-          }
-          printInfo(info: start.value.toString());
-        },
-      );
-    }
+  onResendOtp() async {
+    CommonFunction.loadingShow();
+    ApiRepositories.resenOtp(
+      email: await PreferenceHelper().get(key: PreferenceConstant.USER_EMAIL),
+    ).then((value) async {
+      CommonFunction.loadingHide();
+      if (value is ResponseResendOtp) {
+        CommonFunction.snackbarHelper(message: value.message, isSuccess: true);
+        PreferenceHelper().set(
+          key: PreferenceConstant.USER_EMAIL,
+          value: value.data!.email.toString(),
+        );
+        email.value = MyHelpers.encryptEmailText(value.data?.email ?? "");
+      } else {
+        CommonFunction.snackbarHelper(message: value?.message ?? 'Failed', isSuccess: false);
+      }
+    }, onError: (e) {
+      CommonFunction.loadingHide();
+      CommonFunction.snackbarHelper(message: e.toString(), isSuccess: false);
+    });
   }
 
-  onCompleted(value) {
+  onCompleted(value) async {
     CommonFunction.loadingShow(message: "msg_loading_2");
-    Future.delayed(
-      Duration(seconds: 5),
-      () {
-        CommonFunction.loadingHide();
+    ApiRepositories.validateOtp(
+      email: await PreferenceHelper().get(key: PreferenceConstant.USER_EMAIL),
+      otp: value.toString(),
+    ).then((value) async {
+      CommonFunction.loadingHide();
+      if (value is ResponseValidateOtp) {
+        PreferenceHelper().set(
+          key: PreferenceConstant.USER_EMAIL,
+          value: value.data!.email.toString(),
+        );
         Get.offNamed(Routes.CREATE_PASSWORD);
-      },
-    );
+      } else {
+        CommonFunction.snackbarHelper(message: value?.message ?? 'Failed', isSuccess: false);
+      }
+    }, onError: (e) {
+      CommonFunction.loadingHide();
+      CommonFunction.snackbarHelper(message: e.toString(), isSuccess: false);
+    });
   }
 }
